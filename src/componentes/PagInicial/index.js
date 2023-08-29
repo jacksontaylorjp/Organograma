9c035cabeee89997a5b3544814b506a0660003ac
jacksontaylorjp <jacksontaylorjp@gmail.com>
2023-08-29 11:05:30 -0300

    alteração na forma de atualizar a tela após a exclusão do colaborador

--- src/componentes/PagInicial/index.js
+++ src/componentes/PagInicial/index.js
@@ -7,7 +7,8 @@ const PagInicial = () => {
 
   const [secoes, setSecoes] = useState([]);
   const [colaboradores, setColaboradores] = useState([]);
-
+  //criando um state para atualizar o componente seção após a deletar um colaborador.
+  const [update, setUpdate] = useState(false);
   //atualizando o state com os dados da api
   async function buscaData() {
     const secoesData = await conectaApi.secoes();
@@ -19,11 +20,14 @@ const PagInicial = () => {
   //usando o useEfect para controlar os efeitos colaterias do fetch da api
   useEffect(() => {
     buscaData(); // Chama a função para buscar os dados quando o componente montar
-  }, []); // A dependência vazia garante que o efeito só é executado uma vez após a montagem
+  }, [update]); // A dependência vazia garante que o efeito só é executado uma vez após a montagem
+                //quando o update for alterado, o useEffect será executado novamente.
 
   //atualizando o componente seção após a deletar um colaborador.
-  function deletandoColaborador() {
-    buscaData();
+  function updateTela() {
+      // buscaData(); poderia também ser usado para atualizar a tela, mas com o state ocorre apenas uma busca na api.
+      setUpdate(!update);//quando o colaborador for excluido será chamado a função updateTela, com isso o setUpdate irá receber o oposto do update.
+                        //desse modo, o useEffect será executado novamente, pois esta monitorando o update.Portanto, executando a função buscaData.
   }
 
   return (
@@ -33,7 +37,7 @@ const PagInicial = () => {
                                 tituloSecao={secao.nome} 
                                 corDeFundo={secao.cor} 
                                 colaboradores={colaboradores.filter(colaborador => colaborador.secao === secao.nome)}
-                                aoDeletar={deletandoColaborador}
+                                aoDeletar={updateTela}
                                 />
                                 )}
     </div>
